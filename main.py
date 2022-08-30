b7c848b639f94805abd6e7ece9394d62ff10b32e
User <senamela@wamly.io>
2022-08-30 10:11:53 +0200

    feb b data 200, feb c data attempt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from process_img import process_img
 from datetime import datetime
 
-directory = "./data_collection/feb_data_b/"
+directory = "./data_collection/feb_data_c/"
 filenames = []
 count = 1
 datef = datetime.now().strftime("%d-%m-%y%H:%M:%S")
@@ -24,7 +24,7 @@ print(dir_len)
 for filename in os.listdir(directory):
     if count > dir_len:
         break
-    process_img(f"./data_collection/feb_data_b/{filename}", count, temp_path)
+    process_img(f"./data_collection/feb_data_c/{filename}", count, temp_path)
     count += 1
 
 
